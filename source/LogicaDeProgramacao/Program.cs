0793310eda9395f07f33024d2a8068843f38e8e1
joaokucera <falleirok@gmail.com>
2014-08-10 23:48:16 -0300

    some types
    int, bool, float, double, decimal

--- source/LogicaDeProgramacao/Program.cs
+++ source/LogicaDeProgramacao/Program.cs
@@ -10,6 +10,37 @@ namespace LogicaDeProgramacao
     {
         static void Main(string[] args)
         {
+            // (INT)
+            // inteiro = 10 (número) - INT
+            // A variável abaixo vai guardar um número.
+            int numero = 10;
+
+            int energia = 90;
+            int magia = 40;
+            int soma1 = 100 + 50; // 150
+            int soma2 = energia + magia; // 130
+
+            // (BOOL)
+            // boolean = bool = true ou false
+            bool oPersonagemFoiAtingido = false;
+            bool oPersonagemEstaVivo = true;
+
+            oPersonagemFoiAtingido = true;
+            energia = energia - 1; // 89
+            energia -= 1; // 88
+            energia += 1; // 89
+
+            // (FLOAT)
+            // inteiro = 10
+            // 10,5 - 10.5 ou seja 10...11 flutuante (float)
+            float ouro = 10.5f;
+            double ouro2 = 10.5d;
+            decimal ouro3 = 10.5m;
+            float somatorio = 10.5f + 10.5f + 10.5f;
+            float somatorio2 = ouro + ouro + ouro;
+            float somatorio3 = ouro + (float)ouro2 + (float)ouro3; // CAST (conversão explicita)
+
+
         }
     }
 }
